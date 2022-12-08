06da3e0299e522269f6414fef5a7c67ee06b7627
Blessing Boaslah <boaslah@IdeaPad-3-14IML05-0ff575c5.localdomain>
2022-12-08 13:22:48 +0000

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,50 +3,48 @@
     <footer class="footer">
 
         <div class="footer-row1">
+                <div class="footer-col1">
+                    <h4>CONTACT IFORMATION</h4>
+                    <p>6116 second W Gore Crescent,</p>
+                    <span>Monrovia Liberia West Africa,</span>
+                    <p>+231 778 128 252</p>
+                    <span>Silkroad@Kwagei.com</span><br><br>
+                    <i class="fa-brands fa-instagram"></i><i class="fa-brands fa-facebook-f"></i><i class="fa-brands fa-twitter"></i>
+                </div>
+                <div >
+                    <h4>QUICK LINKS</h4>
 
-             <div class="footer-col1">
-                <h4>CONTACT IFORMATION</h4>
-                <p>6116 second W Gore Crescent,</p>
-                <span>Monrovia Liberia West Africa,</span>
-                <p>+231 778 128 252</p>
-                <span>Silkroad@Kwagei.com</span><br><br>
-                <i class="fa-brands fa-instagram"></i><i class="fa-brands fa-facebook-f"></i><i class="fa-brands fa-twitter"></i>
-            </div>
-            <div >
-                <h4>QUICK LINKS</h4>
-
-                <div class="footer-col2">
-                    <div>
-                        <router-link to="/register">Register</router-link>
-                        <router-link to="/login">Login</router-link>
-                        <router-link to="/">Post Property</router-link>
-                        <router-link to="/">Our Recent Postings</router-link>
-                        <router-link to="/">FAQ</router-link>
-                        <router-link to="/">Contact</router-link>
-                    </div>
-                    <div>
-                        <h4></h4>
-                        <router-link to="/">About Us</router-link>
-                        <router-link to="/">Privacy Policy</router-link>
-                        <router-link to="/">Terms and Conditions</router-link>
+                    <div class="footer-col2">
+                        <div>
+                            <router-link to="/register">Register</router-link>
+                            <router-link to="/login">Login</router-link>
+                            <router-link to="/">Post Property</router-link>
+                            <router-link to="/">Our Recent Postings</router-link>
+                            <router-link to="/">FAQ</router-link>
+                            <router-link to="/">Contact</router-link>
+                        </div>
+                        <div>
+                            <h4></h4>
+                            <router-link to="/">About Us</router-link>
+                            <router-link to="/">Privacy Policy</router-link>
+                            <router-link to="/">Terms and Conditions</router-link>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="footer-col3">
-                <h4>OUR RESENT POSTINGS</h4>
-                <p>3 Bedroom Apartment at Sinkor</p>
-                <span class="top"><i class="fa-sharp fa-solid fa-location-pin"></i>  Sinkor, Monrovia, Liberia</span>
-                <p>3 Bedroom Apartment at Sinkor </p>
-                <span><i class="fa-sharp fa-solid fa-location-pin"></i>  Sinkor, Monrovia, Liberia</span>
-                <p>3 Bedroom Apartment at Sinkor  </p>
-                <span><i class="fa-sharp fa-solid fa-location-pin"></i>  Sinkor, Monrovia, Liberia</span>
-            </div>       
-
-        </div> 
+                <div class="footer-col3">
+                    <h4>OUR RESENT POSTINGS</h4>
+                    <p>3 Bedroom Apartment at Sinkor</p>
+                    <span class="top"><i class="fa-sharp fa-solid fa-location-pin"></i>  Sinkor, Monrovia, Liberia</span>
+                    <p>3 Bedroom Apartment at Sinkor </p>
+                    <span><i class="fa-sharp fa-solid fa-location-pin"></i>  Sinkor, Monrovia, Liberia</span>
+                    <p>3 Bedroom Apartment at Sinkor  </p>
+                    <span><i class="fa-sharp fa-solid fa-location-pin"></i>  Sinkor, Monrovia, Liberia</span>
+                </div>       
+        </div>
         <!-- <div class="footer-row2"></div>    -->
         <div class="footer-row3">
-            <p>Copyright &copy; 2017 - Kwagei Group - All rights reserve</p>
-            <p><b>SILKROAD</b></p>
+            <p>Copyright &copy; 2017 - Kwagei Group - All rights reserve <span class="brand"><b>SILKROAD</b></span></p>
+            
         </div>
     </footer>
 
@@ -61,10 +59,16 @@
 </script>
 
 <style lang="scss" scoped>
+.brand{
+    float: right;
+    font-size: 100%;
+}
+
 .fa-facebook-f{
     margin: 0% 7% 0% 7%;
 }
 .footer-row3{
+    padding: 1% 17% 0% 15%;
     border-top: solid 2px white;
 }
 
@@ -99,7 +103,7 @@ a{
     display: grid;
     grid-template-rows: 80%  20%;
     color: white;
-    padding: 3% 10% 2% 10%;
+    padding: 3% 0% 2% 0%;
     margin: 3% -0.4% -2% -2%;
     row-gap: 5%;
 }
@@ -107,11 +111,9 @@ a{
 .footer-row1{
     display: grid;
     grid-template-columns: 33% 33% 33%;
-    column-gap: 5%;
+    padding: 0% 5% 0% 15%;
 }
-/* .footer-row2{
-    background-color: white;
-} */
+
 @media only screen and (max-width: 600px) {
     .footer{
         margin: 70% -0.4% -2% -2%;
